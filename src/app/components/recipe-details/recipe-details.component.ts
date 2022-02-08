94d2c61ee585bdabdd89f536a8705c689b298020
Monika Sharma <msharma@monmac.local>
2022-02-08 11:35:00 -0800

    updated detail page. added ingredients and instructions

--- src/app/components/recipe-details/recipe-details.component.ts
+++ src/app/components/recipe-details/recipe-details.component.ts
@@ -22,8 +22,34 @@ export class RecipeDetailsComponent implements OnInit {
         'desc': 'Yummieee...',
         'feed_this_many': 4,
         'preparation_time': 60,
-        'ingredients': [{'ingredient': 'Yogurt', 'measure': '2 cup'}],
-        'instructions': [{'instruction': 'Add tea spoon salt in the yogurt', 'photo': ''}],
+        'ingredients': [
+            {'ingredient': 'Yogurt', 'measure': '1/2 cup'},
+            {'ingredient': 'salt', 'measure': '1/2 tsp'},
+            {'ingredient': 'sugar', 'measure': '1 tsp'},
+            {'ingredient': 'red pepper', 'measure': '1 tsp'},
+            {'ingredient': 'black pepper', 'measure': '1 tsp'},
+            {'ingredient': 'chaat masala', 'measure': '1 tsp'},
+            {'ingredient': 'papadi', 'measure': ' 200 gm'},
+            {'ingredient': 'Imali Chatney', 'measure': '1/4 cup'},
+            {'ingredient': 'Green Chatney', 'measure': '1/4 cup'},
+            {'ingredient': 'Celentro chopped', 'measure': ' 1/2 cup'},
+            {'ingredient': 'Hair sev', 'measure': '1/2 cup'},
+          ],
+        'instructions': [
+          {'instruction': 'firstly, in a serving plate take 13 papdi. to prepare homemade papdi using wheat flour check out my papdi recipe.', 'photo': ''},
+          {'instruction': 'top with 3 tbsp potato and 3 tbsp chickpea.', 'photo': ''},
+          {'instruction': 'sprinkle 2 tbsp of chopped onions.', 'photo': ''},
+          {'instruction': 'take 3 tbsp of whisked curd and drizzle over it.', 'photo': ''},
+          {'instruction': 'add 2 tsp of green chutney and 2 tsp of tamarind chutney.', 'photo': ''},
+          {'instruction': 'now add 1 tbsp of more curd, 1 tsp green chutney and 1 tsp tamarind chutney', 'photo': ''},
+          {'instruction': 'top with 3 tbsp sev and garnish with 1 tsp coriander.', 'photo': ''},
+        ],
+
+
+
+
+
+
         'cover_photo': '/assets/indori_chaat.jpeg',
         'keywords': []
       }),
