dcfb0f547882d0f7f811e28858010456be127400
Monika Sharma <msharma@monmac.local>
2022-02-09 11:15:39 -0800

    changes on detail page

--- src/app/components/recipe-details/recipe-details.component.ts
+++ src/app/components/recipe-details/recipe-details.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from 'src/app/model/recipe';
-import { ActivatedRoute, ParamMap} from '@angular/router';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Location } from '@angular/common';
+import { RecipeService } from '../../services/recipe.service';
 
 @Component({
   selector: 'app-recipe-details',
@@ -11,85 +12,27 @@ import { Location } from '@angular/common';
 export class RecipeDetailsComponent implements OnInit {
 
   recipe!: Recipe;
-  recipes: Recipe[];
 
-  constructor(private route: ActivatedRoute, private location: Location) {
-
-    
-    this.recipes = [
-      Recipe.recipeFromJson({
-        'id': 1,
-        'title': 'Indori Chaat',
-        'desc': 'Yummieee...',
-        'feed_this_many': 4,
-        'preparation_time': 60,
-        'ingredients': [
-            {'ingredient': 'Yogurt', 'measure': '1/2 cup'},
-            {'ingredient': 'salt', 'measure': '1/2 tsp'},
-            {'ingredient': 'sugar', 'measure': '1 tsp'},
-            {'ingredient': 'red pepper', 'measure': '1 tsp'},
-            {'ingredient': 'black pepper', 'measure': '1 tsp'},
-            {'ingredient': 'chaat masala', 'measure': '1 tsp'},
-            {'ingredient': 'papadi', 'measure': ' 200 gm'},
-            {'ingredient': 'Imali Chatney', 'measure': '1/4 cup'},
-            {'ingredient': 'Green Chatney', 'measure': '1/4 cup'},
-            {'ingredient': 'Celentro chopped', 'measure': ' 1/2 cup'},
-            {'ingredient': 'Hair sev', 'measure': '1/2 cup'},
-          ],
-        'instructions': [
-          {'instruction': 'firstly, in a serving plate take 13 papdi. to prepare homemade papdi using wheat flour check out my papdi recipe.', 'photo': ''},
-          {'instruction': 'top with 3 tbsp potato and 3 tbsp chickpea.', 'photo': ''},
-          {'instruction': 'sprinkle 2 tbsp of chopped onions.', 'photo': ''},
-          {'instruction': 'take 3 tbsp of whisked curd and drizzle over it.', 'photo': ''},
-          {'instruction': 'add 2 tsp of green chutney and 2 tsp of tamarind chutney.', 'photo': ''},
-          {'instruction': 'now add 1 tbsp of more curd, 1 tsp green chutney and 1 tsp tamarind chutney', 'photo': ''},
-          {'instruction': 'top with 3 tbsp sev and garnish with 1 tsp coriander.', 'photo': ''},
-        ],
-        'cover_photo': '/assets/indori_chaat.jpeg',
-        'keywords': []
-      }),
-      Recipe.recipeFromJson( {
-        'id': 2,
-        'title': 'Jalebi',
-        'desc': 'This is my fav sweets',
-        'feed_this_many': 4,
-        'preparation_time': 30,
-        'ingredients': [{'ingredient': 'Yogurt', 'measure': '2 cup'}],
-        'instructions': [{'instruction': 'Add tea spoon salt in the yogurt', 'photo': ''}],
-        'cover_photo': '/assets/jalebi.jpeg',
-        'keywords': []
-      },)
-    ];
-   }
+  constructor(private route: ActivatedRoute,
+    private location: Location,
+    private recipe_service: RecipeService) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe((params: ParamMap) => {
-      
-      let recipe_id = params.get('recipe_id')
-      let recipe_id_num: number = 0;
 
-      if(recipe_id != null){
-        recipe_id_num = parseInt(recipe_id, 10);
+      let recipe_id_str = params.get('recipe_id')
+      let recipe_id: number = 0;
+
+      if (recipe_id_str != null) {
+        recipe_id = parseInt(recipe_id_str, 10);
       }
-        
-      this.recipe = this.findRecipeById(recipe_id_num);
-     
-      // this.recipe = this.findRecipeById(parseInt(params.get('recipe_id'), 10));
-      console.log(" HERE IS THE JSON: \n",JSON.stringify(this.recipe, null, 2));
+
+      this.recipe_service.getRecipeById(recipe_id)
+        .subscribe((recipe) => this.recipe = recipe);
     });
   }
 
-  findRecipeById(recipe_id: number): Recipe {
-    for(const recipe of this.recipes){
-      if(recipe.id === recipe_id){
-        return recipe;
-      }
-    }
-  
-    return new Recipe(-1, '', '', 1, 1, [], [], '', []);
-  }
-
-  public goBackButtonClciked(){
+  public goBackButtonClciked() {
     this.location.back();
   }
 }
--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -17,22 +17,42 @@ export class RecipeService {
         'desc': 'Yummieee...',
         'feed_this_many': 4,
         'preparation_time': 60,
-        'ingredients': [{ 'ingredient': 'Yogurt', 'measure': '2 cup' }],
-        'instructions': [{ 'instruction': 'Add tea spoon salt in the yogurt', 'photo': '' }],
+        'ingredients': [
+            {'ingredient': 'Yogurt', 'measure': '1/2 cup'},
+            {'ingredient': 'salt', 'measure': '1/2 tsp'},
+            {'ingredient': 'sugar', 'measure': '1 tsp'},
+            {'ingredient': 'red pepper', 'measure': '1 tsp'},
+            {'ingredient': 'black pepper', 'measure': '1 tsp'},
+            {'ingredient': 'chaat masala', 'measure': '1 tsp'},
+            {'ingredient': 'papadi', 'measure': ' 200 gm'},
+            {'ingredient': 'Imali Chatney', 'measure': '1/4 cup'},
+            {'ingredient': 'Green Chatney', 'measure': '1/4 cup'},
+            {'ingredient': 'Celentro chopped', 'measure': ' 1/2 cup'},
+            {'ingredient': 'Hair sev', 'measure': '1/2 cup'},
+          ],
+        'instructions': [
+          {'instruction': 'firstly, in a serving plate take 13 papdi. to prepare homemade papdi using wheat flour check out my papdi recipe.', 'photo': ''},
+          {'instruction': 'top with 3 tbsp potato and 3 tbsp chickpea.', 'photo': ''},
+          {'instruction': 'sprinkle 2 tbsp of chopped onions.', 'photo': ''},
+          {'instruction': 'take 3 tbsp of whisked curd and drizzle over it.', 'photo': ''},
+          {'instruction': 'add 2 tsp of green chutney and 2 tsp of tamarind chutney.', 'photo': ''},
+          {'instruction': 'now add 1 tbsp of more curd, 1 tsp green chutney and 1 tsp tamarind chutney', 'photo': ''},
+          {'instruction': 'top with 3 tbsp sev and garnish with 1 tsp coriander.', 'photo': ''},
+        ],
         'cover_photo': '/assets/indori_chaat.jpeg',
         'keywords': []
       }),
-      Recipe.recipeFromJson({
+      Recipe.recipeFromJson( {
         'id': 2,
         'title': 'Jalebi',
         'desc': 'This is my fav sweets',
         'feed_this_many': 4,
         'preparation_time': 30,
-        'ingredients': [{ 'ingredient': 'Yogurt', 'measure': '2 cup' }],
-        'instructions': [{ 'instruction': 'Add tea spoon salt in the yogurt', 'photo': '' }],
+        'ingredients': [{'ingredient': 'Yogurt', 'measure': '2 cup'}],
+        'instructions': [{'instruction': 'Add tea spoon salt in the yogurt', 'photo': ''}],
         'cover_photo': '/assets/jalebi.jpeg',
         'keywords': []
-      })
+      },)
     ];
     // new Recipe(3, 'Rasmalai', 'Lunch Time...its rasmalai time', 2, 60, [], [],'/assets/rasamalai.jpg', []),
     // new Recipe(4, 'Creme Brulee', 'Benus fav', 6, 40, [], [], '/assets/brulee.jpeg', []),
@@ -44,6 +64,20 @@ export class RecipeService {
     return new Observable<Recipe []>((obserable) => {
       obserable.next(this.recipes);
       obserable.complete();
+    });
+  }
+
+  getRecipeById(recipe_id: number): Observable<Recipe> {
+    return new Observable<Recipe>((obserable) => {
+      for(let recipe of this.recipes){
+        if(recipe.id === recipe_id){
+          obserable.next(recipe);
+          obserable.complete();
+          return;
+        }
+      }
+
+      console.log("OPS cant find given recipe_id " + recipe_id);
     });
   }
 
