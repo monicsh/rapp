a836b20e27f8d1509d09142e6a3792f8ba1dfbf6
Monika Sharma <msharma@monmac.local>
2022-02-11 11:54:20 -0800

    add recipe

--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -1,33 +1,41 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/internal/Observable';
-import { Recipe, RecipesPayload } from '../model/recipe';
-import { HttpClient } from '@angular/common/http';
+import { Recipe, RecipesPayload, RecipePayload } from '../model/recipe';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
-const RECIPE_SERVER = 'http://localhost:8080';
+
+const RECIPE_SERVER = 'http://localhost:4200';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipeService {
 
-  constructor(private http: HttpClient) {}
+  httpOptions: Object;
+
+  constructor(private http: HttpClient) {
+    this.httpOptions = {
+      header: new HttpHeaders({
+        'content-type': 'application/json',
+      }),
+    };
+  }
 
   getAllRecipes(): Observable<RecipesPayload> {
     return this.http.get<RecipesPayload>(RECIPE_SERVER + '/v1/recipes.json');
   }
 
-  getRecipeById(recipe_id: number): Observable<Recipe> {
-    return new Observable();
+  getRecipeById(recipe_id: number): Observable<RecipePayload> {
+    if(recipe_id === undefined){
+      console.log('Ops thats an error');
+      return new Observable();
+    }
+    
+    return this.http.get<RecipePayload>(RECIPE_SERVER + '/v1/recipes/' + recipe_id + '.json');
   }
 
-  addRecipe(recipe: Recipe): number {
-    // const new_recipe_id = this.getNewRecipeId();
-    // recipe.id = new_recipe_id;
-
-    // this.recipes.push(recipe);
-
-    // return new_recipe_id;
-    return -1;
+  addRecipe(recipe: Recipe): Observable<RecipePayload> {
+    return this.http.put<RecipePayload>(RECIPE_SERVER + '/v1/recipes.json', recipe,  this.httpOptions);
   }
 
   // getNewRecipeId(): number {
